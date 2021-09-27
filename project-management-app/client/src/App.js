4dbea319ffc5c11c13cd2246d5b6491391ee6725
Jan Redmann <janredmann1@gmail.com>
2021-09-27 14:42:35 +0200

    add react frontend

---
+++ project-management-app/client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+import { Switch, Route } from 'react-router-dom';
+import ProjectListPage from './pages/ProjectListPage';
+import ProjectDetailsPage from './pages/ProjectDetailsPage';
+import EditProjectPage from './pages/EditProjectPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/projects" component={ProjectListPage} />
+        <Route exact path="/projects/:id" component={ProjectDetailsPage} />
+        <Route exact path="/projects/edit/:id" component={EditProjectPage} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
